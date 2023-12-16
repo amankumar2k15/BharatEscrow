c2086fca4e61474696a34e3c0dd7cae505cf61f5
aman.kumar2k15@gmail.com <aman.kumar2k15@gmail.com>
2023-12-16 10:52:00 +0530

    changes in blog section

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -4,20 +4,23 @@ import Footer from '../components/Footer'
 const Blog = () => {
   return (
     <>
-    <section >
-        <div className='flex flex-col sm:flex-row'>
-            <div className='relative  md:block'>
-                {/* <img src={contactImg} alt='contactImg1' /> */}
-                <p className='uppercase absolute left-20 bottom-16 text-xl sm:text-2xl md:text-5xl'>Contact us</p>
+      <section className='buySell border-y-2 border-white'>
+        <div className=' flex pt-28 pb-[83px] bg-gradient-to-r row from-[#fff] to-[#096aa6]  font-roboto flex-col gap-5 px-2 ' >
+          <div className='font-bold text-3xl sm:text-4xl md:text-5xl mt-8 sm:mt-0 w-full text-center text-blue-900'>
+            BLOG
+          </div>
+        </div>
+        <div className='px-6'>
+          <div className='bg-red-400 h-[400px] w-[400px]'>
+            <h2>No ChargeBack vs Traditional Payment Processors</h2>
+            <div>
+              <img />
             </div>
+          </div>
         </div>
-        <div className='' style={{height:"100vh"}}>
-
-        </div>
-
-    </section>
-    <Footer />
-</>
+      </section>
+      <Footer />
+    </>
   )
 }
 
