6afae13d65019ce40b1b0d380ef26603177a0030
aman.kumar2k15@gmail.com <aman.kumar2k15@gmail.com>
2023-12-16 13:01:21 +0530

    finished blog

---
+++ src/components/SelectedBlog.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SelectedBlog = () => {
+    return (
+        <div>SelectedBlog</div>
+    )
+}
+
+export default SelectedBlog
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -12,39 +12,30 @@ const Blog = () => {
           </div>
         </div>
         <div className=' px-4 md:px-6 w-full py-10'>
-          <div className=' flex flex-row flex-wrap justify-center gap-4'>
+          <div className=' flex flex-row flex-wrap justify-center gap-12'>
 
-            <div className='bg-[#a3c7de] shadow-lg shadow-black hover:-translate-y-1 transition-all duration-300 ease-in-out text-black h-[430px] md:h-[470px] max-w-[400px] px-4 flex flex-col gap-y-4'>
-              <h2 className='text-md mt-5 text-blue-900'>No ChargeBack vs Traditional Payment Processors</h2>
-              <div className='md:h-[250px] md:max-w-[365px]' >
-                <img className='md:h-[260px] md:w-[365px] object-cover' src={blogImg} alt='blogImg' />
+            <div className='bg-[#a3c7de] rounded-lg shadow-lg shadow-black hover:-translate-y-1 transition-all duration-300 ease-in-out text-black h-[430px] md:h-[450px] max-w-[330px] px-4 flex flex-col gap-y-4'>
+              <h2 className='text-md mt-5 text-blue-900 font-bold'>No ChargeBack vs Traditional Payment Processors</h2>
+              <div className='md:h-[200px] max-w-[345px]' >
+                <img className='md:h-[200px] w-[345px] object-cover' src={blogImg} alt='blogImg' />
               </div>
               <p className='text-md mt-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam itaque repudiandae quia officia similique, libero saepe fugiat.</p>
               <button className='text-right text-blue-900 hover:text-black '>Read more...</button>
             </div>
 
-            <div className='bg-[#a3c7de] shadow-lg shadow-black hover:-translate-y-1 transition-all duration-300 ease-in-out text-black h-[430px] md:h-[470px] max-w-[400px] px-4 flex flex-col gap-y-4'>
-              <h2 className='text-md mt-5 text-blue-900'>No ChargeBack vs Traditional Payment Processors</h2>
-              <div className='md:h-[250px] md:max-w-[365px]' >
-                <img className='md:h-[260px] md:w-[365px] object-cover' src={blogImg} alt='blogImg' />
+            <div className='bg-[#a3c7de] rounded-lg shadow-lg shadow-black hover:-translate-y-1 transition-all duration-300 ease-in-out text-black h-[430px] md:h-[450px] max-w-[330px] px-4 flex flex-col gap-y-4'>
+              <h2 className='text-md mt-5 text-blue-900 font-bold'>No ChargeBack vs Traditional Payment Processors</h2>
+              <div className='md:h-[200px] max-w-[345px]' >
+                <img className='md:h-[200px] w-[345px] object-cover' src={blogImg} alt='blogImg' />
               </div>
               <p className='text-md mt-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam itaque repudiandae quia officia similique, libero saepe fugiat.</p>
               <button className='text-right text-blue-900 hover:text-black '>Read more...</button>
             </div>
 
-            <div className='bg-[#a3c7de] shadow-lg shadow-black hover:-translate-y-1 transition-all duration-300 ease-in-out text-black h-[430px] md:h-[470px] max-w-[400px] px-4 flex flex-col gap-y-4'>
-              <h2 className='text-md mt-5 text-blue-900'>No ChargeBack vs Traditional Payment Processors</h2>
-              <div className='md:h-[250px] md:max-w-[365px]' >
-                <img className='md:h-[260px] md:w-[365px] object-cover' src={blogImg} alt='blogImg' />
-              </div>
-              <p className='text-md mt-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam itaque repudiandae quia officia similique, libero saepe fugiat.</p>
-              <button className='text-right text-blue-900 hover:text-black '>Read more...</button>
-            </div>
-
-            <div className='bg-[#a3c7de] shadow-lg shadow-black hover:-translate-y-1 transition-all duration-300 ease-in-out text-black h-[430px] md:h-[470px] max-w-[400px] px-4 flex flex-col gap-y-4'>
-              <h2 className='text-md mt-5'>No ChargeBack vs Traditional Payment Processors</h2>
-              <div className='md:h-[250px] md:max-w-[365px]' >
-                <img className='md:h-[260px] md:w-[365px] object-cover' src={blogImg} alt='blogImg' />
+            <div className='bg-[#a3c7de] rounded-lg shadow-lg shadow-black hover:-translate-y-1 transition-all duration-300 ease-in-out text-black h-[430px] md:h-[450px] max-w-[330px] px-4 flex flex-col gap-y-4'>
+              <h2 className='text-md mt-5 text-blue-900 font-bold'>No ChargeBack vs Traditional Payment Processors</h2>
+              <div className='md:h-[200px] max-w-[345px]' >
+                <img className='md:h-[200px] w-[345px] object-cover' src={blogImg} alt='blogImg' />
               </div>
               <p className='text-md mt-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam itaque repudiandae quia officia similique, libero saepe fugiat.</p>
               <button className='text-right text-blue-900 hover:text-black '>Read more...</button>
