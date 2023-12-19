203804f4bda251b33e2665fd849820ecf8b9eb34
shashank8112357010 <shashanksharma1235999@gmail.com>
2023-12-19 12:58:59 +0530

    added sheet integration

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router'
 
 const ContactForm = () => {
 
+    const navigate = useNavigate()
+    const [contactData, setContactData] = useState({
+        Email: null, Phone: null, Name: null, Message: null
+    })
+
+    const handleSubmitToSpreadSheet = (e) => {
+        e.preventDefault()
+        const formData = new FormData();
+        Object.keys(contactData).forEach((item) => {
+            // console.log(contactData);
+            formData.append(item, contactData[item])
+        })
+        try{
+            const header =  {
+                method: 'POST',
+                mode: 'no-cors', // Set mode to 'no-cors',
+                'Accept': 'application/json, text/plain',
+                'Content-Type': 'application/json;charset=UTF-8',
+                body: formData,
+            }
+            fetch(`https://sheet.best/api/sheets/471a8c9b-3456-48ce-9337-ffb79ec05e3b`,header).then((res)=>{
+                console.log(res)
+                 navigate('/')
+            }).catch((err) => console.log("err====>", err))
+        }catch(err){
+            console.log("Aman" ,  err);
+        }
+      
+    }
+
+
+    // action='https://formspree.io/f/myyqnjdz'
+
     return (
         <>
-            <form action='https://formspree.io/f/myyqnjdz' method='POST'>
+            <form method='POST'  onSubmit={handleSubmitToSpreadSheet}>
                 <div className="relative z-0 w-full mb-6 group">
                     <input
                         type="email"
                         name="Email :-"
                         id="floating_email"
+                        onChange={(e) => setContactData((p) => ({ ...p, Email: e.target.value }))}
                         className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                         placeholder=""
                         required
@@ -29,6 +64,7 @@ const ContactForm = () => {
                             type="text"
                             name="Your Name :-"
                             id="floating_first_name"
+                            onChange={(e) => setContactData((p) => ({ ...p, Name: e.target.value }))}
                             className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                             placeholder=" "
                             required
@@ -44,6 +80,7 @@ const ContactForm = () => {
                         <input
                             type="number"
                             name="Phone Number :-"
+                            onChange={(e) => setContactData((p) => ({ ...p, Phone: e.target.value }))}
                             id="floating_phone"
                             className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                             placeholder=""
@@ -63,6 +100,7 @@ const ContactForm = () => {
                         <textarea
                             type="text"
                             name="Textarea"
+                            onChange={(e) => setContactData((p) => ({ ...p, Message: e.target.value }))}
                             id="Textarea"
                             className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                             placeholder=" "
@@ -72,7 +110,7 @@ const ContactForm = () => {
                             htmlFor="Textarea"
                             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                         >
-                            Textarea
+                            Message 
                         </label>
                     </div>
                 </div>
