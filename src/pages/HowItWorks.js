432af38c91ddcdbaaf7f7e777a0928fb05043ac5
aman.kumar2k15@gmail.com <aman.kumar2k15@gmail.com>
2023-12-19 13:50:19 +0530

    changes in howtiworkd

--- src/pages/HowItWorks.js
+++ src/pages/HowItWorks.js
@@ -9,56 +9,86 @@ import Footer from '../components/Footer';
 
 const sections = [
     {
-        title: "Create an account Signup/Login",
-        content: "It's easy as buying the goods/services from online store. A buyer and seller open an escrow account on our site once they decide to complete a transaction. The buyer shows their commitment by depositing the agreed-upon sum of money into the escrow account. The seller continues to carry out their obligations as stated in the agreement in the meantime.",
+        id: 0,
+        title: "Registration with BharatEscrow",
+        content: "Begin by registering on the BharatEscrow platform. Users can easily create an account, providing necessary details for identity verification. This initial step establishes the foundation for secure and transparent financial transactions. We generally ask for the basic KYC and KYB documents from each clients to completed the verfication process.",
         image: loginSignup,
         reversed: false
     },
     {
-        title: "Certifying Buyer",
-        content: "Our team continuously monitors the transaction throughout to make sure that both sides carry out their obligations in the stipulated time frame. The money is released to the vendor after the buyer certifies that they have got the goods or service as promised. This technique protects both parties from potential fraud and deception. since, the transaction are routed through a secure channel and both parties can carried out the job as per the agreed agreement.",
+        id: 1,
+        title: "Agreement Initiation",
+        content: "Once registered, initiate an agreement on the BharatEscrow platform. Clearly outline the terms and conditions of the transaction, including payment details, milestones, and delivery expectations. This agreement serves as the blueprint for the entire escrow process wherein each information from beginning of the project to completeion of the project.",
         image: CertifyingBuyer,
         reversed: true
     },
     {
-        title: "Multiple Payment Methods",
-        content: "We are aware that each transaction is distinct. We provide workable alternatives as a result to meet multiple payment options for both parties. Our platform enables you to tailor the terms based on your unique needs, whether you require a one-time payment or prefer recurring payments. We work to develop financial agreements customized to your needs because we believe they should be as every business have a unique needs and requirements.",
+        id: 2,
+        title: "Fund Transfer to Escrow Account",
+        content: "Upon agreement finalization, the buyer transfers the agreed-upon funds to the RBI-regulated escrow account facilitated by BharatEscrow. This step ensures that the funds are securely held in escrow until all conditions are met, instilling confidence in both parties. There would be few instances where the settlement would be based on the milestone and such case will be only occured in freelanceing projects done either by freelancer or a small firms.",
         image: MultiplePayment,
         reversed: false
     },
     {
-        title: "Security & Privacy",
-        content: "We are steadfast in our commitment to security and privacy policy of our clientel. With the aid of our RBI-approved banking partners, we use cutting-edge encryption technology and stringent verification procedures to protect your personal and financial information. You can rely on BharatEscrow to always protect your sensitive information. We categorically do mean 'convenient, secure and reliable' when we use that word.",
+        id: 3,
+        title: "Verification and Approval",
+        content: "BharatEscrow, in collaboration with its banking partners, conducts thorough verification processes. This includes confirming the authenticity of the funds transferred and validating the compliance of the transaction with regulatory standards. Once verified, the funds are approved for release upon fulfillment of the agreed conditions. we need to make sure that the funds are transferred through a designated account.",
         image: Security,
         reversed: true
     },
     {
-        title: "Excellent Customer Service",
-        content: "In addition to our robust security measures, we also prioritize In addition to our robust security measures, we also prioritize excellent customer service. Our dedicated realtionship manager is available to assist you every step of the way, addressing any concerns or queries you may have. We value your satisfaction and aim to provide a seamless experience on our platform. Experience a new level of trust and security in your online transactions by signing up with BharatEscrow today. We're here to safeguard your financial interests and make sure that your transactions are conducted with trust, whether you're a buyer or a seller.",
+        id: 4,
+        title: "Milestone Tracking and Updates",
+        content: "Throughout the transaction, BharatEscrow provides a transparent milestone tracking system. Regular updates are communicated to both the buyer and seller, ensuring everyone is informed about the progress and any necessary actions required to move the transaction forward. This sort of payment will only occurs where a firm/freelancers are developing a project for a sepicifc needs .",
         image: Services,
         reversed: false
+    },
+    {
+        id: 5,
+        title: "Conditional Release of Funds",
+        content: "Upon successful completion of the agreed-upon conditions, the funds held in escrow are released to the seller. This step ensures that both parties adhere to their commitments, creating a secure and fair transaction environment.",
+        image: MultiplePayment,
+        reversed: true
     },
 
 ];
 
-const Section = ({ title, content, image, reversed }) => (
-    <div className={`flex flex-col justify-center items-center ${reversed ? "sm:flex-row-reverse" : "sm:flex-row"} px-2 sm:flex-wrap md:flex-nowrap sm:justify-evenly`}>
-        <div className='flex flex-col justify-center gap-4 mb-8 w-11/12 sm:w-[300px] md:w-[400px] mx-2 md:px-6'>
-            <h2 className='text-lg font-semibold sm:text-xl md:text-2xl underline text-blue-900'>{title}</h2>
-            <h3 className='text-sm sm:text-md md:text-lg text-justify'>{content}</h3>
+const Section = ({ title, id, content, image, reversed }) => {
+    return (
+        <div className={`flex flex-col justify-center items-center ${reversed ? "sm:flex-row-reverse" : "sm:flex-row"} px-4 sm:flex-wrap md:flex-nowrap gap-y-4 sm:justify-evenly`}>
+            <div className='flex flex-col justify-center gap-4 mb-8 w-11/12  md:w-[500px] mx-2 md:px-6'>
+                <h2 className='text-lg inline-flex font-semibold sm:text-xl md:text-2xl whitespace-nowrap text-blue-900'>
+                    <div className='no-underline '>{`Step ${id + 1}: `}</div>
+                    &nbsp;{title}
+                </h2>
+                <h3 className='text-sm sm:text-md md:text-lg text-justify'>{content}</h3>
+            </div>
+            <div className='sm:w-[400px] md:w-[450px] h-[250px] md:h-[400px] px-5 sm:px-0'>
+                <img className='object-cover sm:w-[400px] md:w-[450px] h-[250px] md:h-[400px]' src={image} alt={title} />
+            </div>
         </div>
-        <div className='sm:w-[400px] md:w-[450px] h-[250px] md:h-[400px] px-5 sm:px-0'>
-            <img className='object-cover sm:w-[400px] md:w-[450px] h-[250px] md:h-[400px]' src={image} alt={title} />
-        </div>
-    </div>
-);
+    )
+};
 
 const HowItWorks = () => (
     <section className='howItWork'>
-        <div className='max-w-4xl mt-[104px] flex flex-col mx-auto text-black md:px-3 '>
+        <div className='max-w-5xl mt-[104px] flex flex-col mx-auto text-black md:px-3 '>
             <div className='mt-14 mb-4'>
                 <h1 className=' text-blue-900 font-bold text-2xl  sm:text-3xl md:text-4xl lg:text-5xl text-center font-roboto'>How does BharatEscrow work?</h1>
             </div>
+
+            <div className='px-10 py-8'>
+                <h2 className='text-lg font-semibold sm:text-xl md:text-2xl underline text-blue-900'>What is an Escrow Account?</h2>
+                <p className='text-sm sm:text-md md:text-lg text-justify'>It is an intermittent account that is only active till the transaction is completed. The account is closed once all of the criteria between the buyer and seller have been met. Escrows often involve the deposit of a monetary-valued document. These documents could be deeds, written instruments, payment pledges, licenses, patents, cheques, bonds, or mortgages.</p>
+            </div>
+
+            <div className='px-10 py-4'>
+                <h2 className='text-lg font-semibold sm:text-xl md:text-2xl underline text-blue-900'>Details about Escrow Account?</h2>
+                <p className='text-sm sm:text-md md:text-lg text-justify'>In an era where financial transparency is paramount, the use of escrow accounts has become a cornerstone for secure transactions. BharatEscrow, in collaboration with multiple RBI-regulated banking partners, is at the forefront of delivering unparalleled financial transparency between buyers and sellers. This comprehensive guide outlines the step-by-step process of utilizing an escrow account with BharatEscrow, aligning with the vision of providing financial transparency to everyone.</p>
+            </div>
+
+
+            {/* sections mapping data  */}
             {sections.map((section, index) => <Section key={index} {...section} />)}
         </div>
         <GetInTouch />
