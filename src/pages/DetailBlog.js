af486b7993a062304dafae89c9806d85eb0caa02
shashank8112357010 <shashanksharma1235999@gmail.com>
2023-12-19 17:39:31 +0530

    added blog page

--- src/pages/DetailBlog.js
+++ src/pages/DetailBlog.js
@@ -231,6 +231,62 @@ const DetailBlog = ({ data }) => {
               </div>
 
               <div className='mt-8'>
+                <p className='text-blue-900 mb-6 font-bold text-2xl font-roboto'>FAQ's</p>
+
+                {
+                  displayData?.faqs.map((item, index) => {
+                    return (
+                      <div id="accordion-collapse" data-accordion="collapse" onClick={() => setRow(index)}>
+                        <h2 id="accordion-collapse-heading-1">
+                          <button
+                            type="button"
+                            className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                            data-accordion-target="#accordion-collapse-body-1"
+                            aria-expanded="true"
+                            aria-controls="accordion-collapse-body-1"
+                          >
+                            <span>{item.doubt}</span>
+                            <svg
+                              data-accordion-icon=""
+                              className="w-3 h-3 rotate-180 shrink-0"
+                              aria-hidden="true"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 10 6"
+                            >
+                              <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M9 5 5 1 1 5"
+                              />
+                            </svg>
+                          </button>
+                        </h2>
+                        <div
+                          id="accordion-collapse-body-1"
+                          className={faqRow === index ? "visible bg-slate-100" : "hidden"}
+                          aria-labelledby="accordion-collapse-heading-1"
+                        >
+                          <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+                            <p className="mb-2 text-gray-500 dark:text-gray-400">
+                              {item.solution}
+                            </p>
+
+                          </div>
+                        </div>
+
+                      </div>
+                    )
+                  })
+
+                }
+
+              </div>
+
+
+              <div className='mt-8'>
                 <p className='text-blue-900 font-bold text-2xl font-roboto'>Conclusion</p>
                 <p className='text-gray-600 text-sm font-roboto'>{displayData?.conclusion}</p>
               </div>
