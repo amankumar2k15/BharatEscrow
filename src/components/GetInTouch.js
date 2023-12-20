1f4b1c88de37fd729624c2b29ebf45cd8113a74d
shashank8112357010 <shashanksharma1235999@gmail.com>
2023-12-20 10:31:22 +0530

    scroll bar is back + blog faq's is fixed

--- src/components/GetInTouch.js
+++ src/components/GetInTouch.js
@@ -16,13 +16,13 @@ const GetInTouch = () => {
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d218.96412105256246!2d77.07323157128543!3d28.586996230465697!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1bee299b98b5%3A0xd5e467baf55d19a8!2sE%20596%2C%20Rd%20No.%20224%2C%20Sector%207%2C%20Block%20F%2C%20Palam%20Extension%2C%20Palam%2C%20New%20Delhi%2C%20Delhi%2C%20110077!5e0!3m2!1sen!2sin!4v1694668395599!5m2!1sen!2sin"
                         style={{ border: "0" }}
                         allowFullScreen=""
-                        loading="lazy"
+                        loading=''
                         referrerPolicy="no-referrer-when-downgrade"
                         className=' h-[300px] w-full px-4 md:w-[600px]'
                     />
 
                     {/* right  */}
-                    <div className='w-full  sm:w-[400px] px-4 sm:px-0 h-[200px] gap-5'>
+                    <div className='w-full  sm:w-[400px] px-4 sm:px-0 h-[100px] gap-3'>
                         <div className='flex flex-col gap-5'>
                             <div className='text-2xl text-bold text-blue-900'>Head office</div>
                             <p className='text-l text-[#000]'>E596, 4 floor, Daani plaza, Ramphal chowk, sector 7, Dwarka, New Delhi 110077</p>
